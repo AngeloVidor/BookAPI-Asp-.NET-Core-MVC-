7c562cb086f16c558fe6110ea79fbe02134edfaf
angel <you@example.com>
2023-11-29 01:12:20 -0300

    Adicionar arquivos de projeto.

---
+++ BookAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BookAPI.Models;
+
+namespace BookAPI.Data
+{
+    public class ApplicattionDbContext : DbContext
+    {
+        public ApplicattionDbContext(DbContextOptions<ApplicattionDbContext> options) : base(options)
+        {
+        }
+
+
+        public DbSet<EmprestimosModel> Emprestimos { get; set; }
+    }
+
+}
